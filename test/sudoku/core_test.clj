99899f2c85471041f574e1a634a90a9834a18e23
= <sassela@live.com>
2016-02-21 11:39:24 +0000

    Test singleton?

--- test/sudoku/core_test.clj
+++ test/sudoku/core_test.clj
@@ -7,3 +7,10 @@
   (testing "function transforms a vector of numbers into a vector of sets"
     (let [transformed-line [#{7 1 4 6 3 2 9 5 8} #{2} #{5} #{7 1 4 6 3 2 9 5 8}]]
           (is (= transformed-line (transform-line [0 2 5 0]))))))
+
+(deftest singleton?-test
+  (testing "function determines whether the argument is a singleton"
+    (true? (singleton? #{3}))
+    (false? (singleton? [3]))
+    (false? (singleton? #{"3"}))
+    (false? (singleton? #{3 5}))))
