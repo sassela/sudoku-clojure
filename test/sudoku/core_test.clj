28809150ed71052cdbd9c6d4a74cb2eaa7e03bc6
= <sassela@live.com>
2016-02-21 12:56:54 +0000

    Test remove-singleton

--- test/sudoku/core_test.clj
+++ test/sudoku/core_test.clj
@@ -5,8 +5,8 @@
 
 (deftest transform-line-test
   (testing "function transforms a vector of numbers into a vector of sets"
-    (let [transformed-line [#{7 1 4 6 3 2 9 5 8} #{2} #{5} #{7 1 4 6 3 2 9 5 8}]]
-          (is (= transformed-line (transform-line [0 2 5 0]))))))
+    (let [transformed-line [#{7 1 4 6 3 2 9 5 8} #{2} #{5}]]
+          (is (= transformed-line (transform-line [0 2 5]))))))
 
 (deftest singleton?-test
   (testing "function determines whether the argument is a singleton"
@@ -14,3 +14,8 @@
     (is (false? (singleton? [3])))
     (is (false? (singleton? #{"3"})))
     (is (false? (singleton? #{3 5})))))
+
+(deftest remove-singleton-test
+  (testing "function removes a singleton from a set of numbers"
+    (let [line [#{7 1 4 6 3 2 9 5 8} #{5} #{9}]]
+      (is (= [#{7 1 4 6 3 2 5 8} #{5} #{9}] (remove-singleton #{9} line))))))
